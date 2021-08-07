d47d00cc53e6533fd5bc7f1dc61ed59cd26f7b78
YoussefWilliam <youssef.william997@gmail.com>
2021-08-07 12:27:02 +0200

    Adding post routers

--- server/index.js
+++ server/index.js
@@ -1,4 +1,28 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+const CONNCETION_URL =
+  process.env.CONNCETION_URL ||
+  "mongodb+srv://YoussefWilliam:YoussefWilliam123@memory-mern.epgnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNCETION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(` 🚀 Server running on port ${PORT} 🚀`))
+  )
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
