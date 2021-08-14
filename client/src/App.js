96229e616e0d59d917dab0ee94751aa73480ed14
YoussefWilliam <youssef.william997@gmail.com>
2021-08-14 12:17:35 +0200

    Adding styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/logo.png";
+import Forms from "./components/Forms/Forms";
+import Posts from "./components/Posts/Posts";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" width="220" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" align="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Forms />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/Forms/Forms.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Forms = () => {
+  return <h1>FORMS</h1>;
+};
+
+export default Forms;
